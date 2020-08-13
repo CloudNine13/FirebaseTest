2809102fe82887ed6f448ba5ff118eef999f0de8
Igor Dzichkovskii <Igordzich@gmail.com>
2020-08-13 19:07:57 +0200

    Initial Commit

---
+++ FirebaseTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseTest
+//
+//  Created by Vitcord23 on 13/08/2020.
+//  Copyright © 2020 IgorDzichkovskii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
